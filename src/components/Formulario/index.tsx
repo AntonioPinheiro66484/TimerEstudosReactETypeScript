4fee30c43995cb99fb775f420795dfa5f4c1360e
HadeSatanist <ap66484@gmail.com>
2023-01-15 15:55:07 -0300

    No index do form esta sendo implementado de forma dinamica a adção das tarefas, usando setState e funcoes

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -6,9 +6,14 @@ class Formulario extends React.Component{
         tarefa :'',
         tempo :'00:00:00'
     }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        console.log('state:', this.state);
+    }
     render(){
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
@@ -16,6 +21,8 @@ class Formulario extends React.Component{
                     <input 
                     type="text"
                     name="tarefa"
+                    value={this.state.tarefa}
+                    onChange={evento=> this.setState({...this.state, tarefa:evento.target.value})}
                     id="tarefa"
                     placeholder="O que você quer estudar "
                     required
@@ -30,6 +37,8 @@ class Formulario extends React.Component{
                     type="time"
                     step="1"
                     name="tempo"
+                    value={this.state.tempo}
+                    onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                     id="tempo"
                     min="00:00:00"
                     max="01:30:00"
