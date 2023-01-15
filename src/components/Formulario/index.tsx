0ccb9f650c232769653fe30b26f654bd66303b24
HadeSatanist <ap66484@gmail.com>
2023-01-15 16:28:30 -0300

    ajuste no formulario colocando os Arrays de forma tipadas e importando uma interface com os tipos do objeto novo criado

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { ITarefa } from '../../types/tarefa'
 import Botao from '../Botao'
 import style from './Formulario.module.scss'
-class Formulario extends React.Component{
+
+class Formulario extends React.Component<{
+    setTarefas:React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
     state={
         tarefa :'',
         tempo :'00:00:00'
@@ -9,7 +13,7 @@ class Formulario extends React.Component{
 
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
         evento.preventDefault();
-        console.log('state:', this.state);
+        this.props.setTarefas(tarefasAntigas=>[...tarefasAntigas,{...this.state}])
     }
     render(){
         return(
@@ -45,8 +49,8 @@ class Formulario extends React.Component{
                     required
                     />
                 </div>
-                <Botao
-                texto="Adicionar"
+                <Botao 
+                texto="Adicionar" type="submit"
                 />
             </form>
         )
