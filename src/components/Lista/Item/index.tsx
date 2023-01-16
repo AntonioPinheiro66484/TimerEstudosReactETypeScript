75f9295509c984b39404d847c72fa836c479660c
HadeSatanist <ap66484@gmail.com>
2023-01-16 12:29:10 -0300

    criando o metodo de finalizar a tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -15,8 +15,8 @@ export default function Item ({
 } : Props){
     console.log('item atual:',{tarefa, tempo, selecionado, completado, id})
     return(
-    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`}
-        onClick={()=> selecionaTarefa(
+    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${completado ? style.itemCompletado:''}`}
+        onClick={()=> !completado && selecionaTarefa(
         {
             tarefa,
             tempo,
@@ -27,6 +27,7 @@ export default function Item ({
     >
         <h3>{tarefa}</h3>
         <span>{tempo}</span>
+        {completado && <span className={style.concluido} aria-label='tarefa completada'></span>}
     </li>
     )
 }
